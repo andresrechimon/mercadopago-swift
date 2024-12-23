948e0f0164fd31619224bd563e37a48c1160a607
Andres Rechimon <nbk2302@oca.uy.corp.itaubank.com>
2024-12-23 18:01:49 -0300

    Initial Commit

---
+++ mercadopago-home-swift/mercadopago_home_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  mercadopago_home_swiftApp.swift
+//  mercadopago-home-swift
+//
+//  Created by Andres Rechimon on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct mercadopago_home_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
