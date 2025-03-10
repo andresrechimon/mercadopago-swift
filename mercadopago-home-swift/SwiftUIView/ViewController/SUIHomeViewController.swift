bc032821124774e2353805372c618d613d3855f3
Andres Rechimon <nbk2302@oca.uy.corp.itaubank.com>
2025-03-10 13:58:47 -0300

    Background gradient refactored

--- mercadopago-home-swift/SwiftUIView/ViewController/SUIHomeViewController.swift
+++ mercadopago-home-swift/SwiftUIView/ViewController/SUIHomeViewController.swift
@@ -10,16 +10,7 @@ import SwiftUI
 struct SUIHomeViewController: View {
     var body: some View {
         ZStack {
-            LinearGradient(
-                gradient: Gradient(stops: [
-                    .init(color: Color(uiColor: UIColor.lightBlue), location: 0.0),
-                    .init(color: Color(uiColor: UIColor.lightBlue), location: 0.15),
-                    .init(color: Color(uiColor: UIColor.grayBackground), location: 0.40)
-                ]),
-                startPoint: UnitPoint(x: 0.5, y: 0.0),
-                endPoint: UnitPoint(x: 0.5, y: 1.0)
-            )
-            .edgesIgnoringSafeArea(.all)
+            LightBlueGradientView()
         }
     }
 }
---
+++ mercadopago-home-swift/SwiftUIView/Views/Background/LightBlueGradientView.swift
@@ -0,0 +1,27 @@
+//
+//  LightBlueGradientView.swift
+//  mercadopago-home-swift
+//
+//  Created by Andres Rechimon on 10/03/2025.
+//
+
+import SwiftUI
+
+struct LightBlueGradientView: View {
+    var body: some View {
+        LinearGradient(
+            gradient: Gradient(stops: [
+                .init(color: Color(uiColor: UIColor.lightBlue), location: 0.0),
+                .init(color: Color(uiColor: UIColor.lightBlue), location: 0.15),
+                .init(color: Color(uiColor: UIColor.grayBackground), location: 0.40)
+            ]),
+            startPoint: UnitPoint(x: 0.5, y: 0.0),
+            endPoint: UnitPoint(x: 0.5, y: 1.0)
+        )
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+#Preview {
+    LightBlueGradientView()
+}
