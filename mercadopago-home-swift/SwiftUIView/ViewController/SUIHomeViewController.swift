662871b9595b3035cc7d3088db857177cda3f67d
Andres Rechimon <nbk2302@oca.uy.corp.itaubank.com>
2025-03-10 13:29:43 -0300

    light blue gradient for background

--- mercadopago-home-swift/SwiftUIView/ViewController/SUIHomeViewController.swift
+++ mercadopago-home-swift/SwiftUIView/ViewController/SUIHomeViewController.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct SUIHomeViewController: View {
     var body: some View {
-        Text("Hello, Home!")
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(stops: [
+                    .init(color: Color(uiColor: UIColor.lightBlue), location: 0.0),
+                    .init(color: Color(uiColor: UIColor.lightBlue), location: 0.15),
+                    .init(color: Color(uiColor: UIColor.grayBackground), location: 0.40)
+                ]),
+                startPoint: UnitPoint(x: 0.5, y: 0.0),
+                endPoint: UnitPoint(x: 0.5, y: 1.0)
+            )
+            .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
